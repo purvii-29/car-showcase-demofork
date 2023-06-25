cb0006db20560ccc48c268e1ed7db4112ca8cc70
alexeerma <aleksander.eerma@hotmail.com>
2023-06-25 20:40:12 +0300

    Added some imports to work on CarDetails.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import Image from 'next/image';
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { CarProps } from '@/types';
 
-const CarDetails = () => {
+interface CarDetailProps {
+    isOpen:boolean;
+    closeModal: () => void;
+    car: CarProps;
+}
+
+const CarDetails = ({isOpen, closeModal, car}: CarDetailProps) => {
   return (
     <div>CarDetails</div>
   )
