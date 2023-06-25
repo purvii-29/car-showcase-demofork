e6f992fcafa28d8791f79c94eb26ecef73ff62ca
alexeerma <aleksander.eerma@hotmail.com>
2023-06-25 11:59:45 +0300

    Added layout logic and states to fetch data through API

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
 
   return (
     <div className='search-manufacturer'>
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
             <div className='relative w-full'>
                 <Combobox.Button className="absolute top-[14px]">
                     <Image
@@ -52,14 +52,46 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
                     afterLeave={() => setQuery('')}
                 >
                     <Combobox.Options>
+                        {filteredManufacturers.length === 0 && query !== "" ? (
+                            <Combobox.Option
+                                value={query}
+                                className="search-manufacturer__option"
+                            >
+                                Create "{query}"
+                            </Combobox.Option>
+                        ) : (
+                            filteredManufacturers.map((item) => (
+                              <Combobox.Option
+                                key={item}
+                                className={({ active }) =>
+                                  `relative search-manufacturer__option ${
+                                    active ? "bg-primary-blue text-white" : "text-gray-900"
+                                  }`
+                                }
+                                value={item}
+                              >
+                                {({ selected, active }) => (
+                                 <>
+                                    <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                                      {item}
+                                    </span>
 
+                                    {/* Show an active blue background color if the option is selected */}
+                                    {selected ? (
+                                      <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                                      ></span>
+                                    ) : null}
+                                 </>
+                                )}
+                                </Combobox.Option>
+                            ))
+                        )}
                     </Combobox.Options>
-
                 </Transition>
             </div>
         </Combobox>
     </div>
-  ) 
-}
+  ); 
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
