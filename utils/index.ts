f8a51a7e8f50ed6a2d5dac1aa68ac5c0d6d6e9a9
alexeerma <aleksander.eerma@hotmail.com>
2023-06-26 22:20:42 +0300

    Added car image URL util

--- utils/index.ts
+++ utils/index.ts
@@ -26,3 +26,18 @@ export async function fetchCars() {
     const result = await response.json();
     return result;
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+  } 
